68182338737d18605c5d24f6ddec208c00c2694c
Nelson Senna <onelsonsenna@gmail.com>
2013-10-04 04:21:59 -0300

    Use locale for every I18n.l call
    Patched this because I was having trouble with pt-BR locale.
    I simply changed I18n.l to use third parameter ':locale' with
    LOCALE user defined variable.

--- _plugins/i18n_filter.rb
+++ _plugins/i18n_filter.rb
@@ -1,6 +1,6 @@
 require 'i18n'
 
-LOCALE = :lv # set your locale
+LOCALE = 'lv' # set your locale
 
 # Create folder "_locales" and put some locale file from https://github.com/svenfuchs/rails-i18n/tree/master/rails/locale
 module Jekyll
@@ -11,13 +11,12 @@ module Jekyll
     def localize(input, format=nil)
       load_translations
       format = (format =~ /^:(\w+)/) ? $1.to_sym : format
-      I18n.l input, :format => format
+      I18n.l input, :format => format, :locale => LOCALE
     end
 
     def load_translations
       unless I18n::backend.instance_variable_get(:@translations)
         I18n.backend.load_translations Dir[File.join(File.dirname(__FILE__),'../_locales/*.yml')]
-        I18n.locale = LOCALE
       end
     end
   end
